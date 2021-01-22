6d9f4ecc1164b1162a1a7f1abb6e54db3bc7756b
Rmiverson <Rmiverson99@gmail.com>
2021-01-21 16:54:18 -0800

    done

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = (arr) => {
+  let win = arr.find(obj => {return obj.result === "W"})
+  
+  if (!win) {
+    return undefined
+  } else {
+    return win.year
+  }
+}
+
